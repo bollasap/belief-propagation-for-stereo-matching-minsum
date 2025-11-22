10b29c6d521a42a060762aedf9915129a93e8993
bollasap <apbollas@hotmail.com>
2025-11-22 21:52:58 +0200

    Add files via upload

--- MinSum.m
+++ MinSum.m
@@ -17,15 +17,15 @@ right = imgaussfilt(right,0.6,'FilterSize',5);
 % Get the image size
 [height,width] = size(left);
 
-% Cache data cost
-dataCost = zeros(height,width,dispLevels);
+% Compute data term
+dataTerm = zeros(height,width,dispLevels);
 for i = 1:dispLevels
 	right_d = [zeros(height,d(i)),right(:,1:end-d(i))];
-	dataCost(:,:,i) = abs(double(left)-double(right_d));
+	dataTerm(:,:,i) = abs(double(left)-double(right_d));
 end
 
-% Cache smoothness cost
-smoothnessCost = lambda*min(abs(d-d'),threshold);
+% Compute smoothness term
+smoothnessTerm = lambda*min(abs(d-d'),threshold);
 
 % Initialize messages
 msgUp = zeros(height,width,dispLevels);
@@ -38,42 +38,42 @@ figure
 % Start iterations
 for i = 1:iterations
 	% Auxiliary tables that help us create the messages
-	U = dataCost + msgDown + msgRight + msgLeft;
-	D = dataCost + msgUp + msgRight + msgLeft;
-	R = dataCost + msgUp + msgDown + msgLeft;
-	L = dataCost + msgUp + msgDown + msgRight;
+	U = dataTerm + msgDown + msgRight + msgLeft;
+	D = dataTerm + msgUp + msgRight + msgLeft;
+	R = dataTerm + msgUp + msgDown + msgLeft;
+	L = dataTerm + msgUp + msgDown + msgRight;
 	
 	% For each pixel
 	for y = 2:height-1
 		for x = 2:width-1
 			% Send message up
 			msg = reshape(U(y,x,:),[dispLevels,1]);
-			msg = min(msg+smoothnessCost);
+			msg = min(msg+smoothnessTerm);
 			msg = msg-min(msg);
 			msgDown(y-1,x,:) = msg;
 			
 			% Send message down
 			msg = reshape(D(y,x,:),[dispLevels,1]);
-			msg = min(msg+smoothnessCost);
+			msg = min(msg+smoothnessTerm);
 			msg = msg-min(msg);
 			msgUp(y+1,x,:) = msg;
 			
 			% Send message right
 			msg = reshape(R(y,x,:),[dispLevels,1]);
-			msg = min(msg+smoothnessCost);
+			msg = min(msg+smoothnessTerm);
 			msg = msg-min(msg);
 			msgLeft(y,x+1,:) = msg;
 			
 			% Send message left
 			msg = reshape(L(y,x,:),[dispLevels,1]);
-			msg = min(msg+smoothnessCost);
+			msg = min(msg+smoothnessTerm);
 			msg = msg-min(msg);
 			msgRight(y,x-1,:) = msg;
 		end
 	end
 	
 	% Compute belief
-	belief = dataCost + msgUp + msgDown + msgRight + msgLeft;
+	belief = dataTerm + msgUp + msgDown + msgRight + msgLeft;
 	
 	% Update disparity map
 	[Y,I] = min(belief,[],3);
